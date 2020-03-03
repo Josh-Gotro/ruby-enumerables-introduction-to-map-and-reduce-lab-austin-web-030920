6958a8ac2e24be5b0285c2ed7ddab96acf9a9e49
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-03-03 19:36:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,7 +27,10 @@ def reduce_to_total(source_array, starting_point=nil)
     source_array.map {|e| ttl +=  e}
     ttl
 end
- def reduce_to_all_true
+
+def reduce_to_all_true(source_array)
+   
+end
 
 
 
